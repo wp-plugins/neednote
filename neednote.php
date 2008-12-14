67f3dbe07350e873e82da7bc1444c7132124a070
peppolone <peppolone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-14 16:25:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/neednote/trunk@79977 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- neednote.php
+++ neednote.php
@@ -42,7 +42,6 @@ $neednote_files = Array(
 	'description_selection.js',
 	'neednote-admin.css',
 	'neednote.php',
-	'wists.js',
 	'images/',
 	'images/1.gif',
 	'images/2.gif',
@@ -198,12 +197,6 @@ if (is_array($neednote_contitionals) and in_array(true, $neednote_contitionals))
 
 // Hook wp_head to add css
 add_action('wp_head', 'neednote_wp_head');
-function neednote_wp_head() {
-	global $neednotepluginpath;
-	if (in_array('Wists', get_option('neednote_active_sites'))) {
-		echo '<script language="JavaScript" type="text/javascript" src="' . $neednotepluginpath . 'wists.js"></script>'."\n";
-	}
-}
 
 // load wp rss functions for update checking.
 if (!function_exists('parse_w3cdtf')) {
